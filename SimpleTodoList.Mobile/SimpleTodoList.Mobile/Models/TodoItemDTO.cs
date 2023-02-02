3438912c3b40637337e1bbc350ffba4f94c4cade
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-02-02 16:46:52 -0600

    Added and Implemented CreateNewTodoItem Command

---
+++ SimpleTodoList.Mobile/SimpleTodoList.Mobile/Models/TodoItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleTodoList.Mobile.Models
+{
+    public class TodoItemDTO
+    {
+        public int Id { get; set; } 
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ SimpleTodoList.Mobile/SimpleTodoList.Mobile/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using SimpleTodoList.Mobile.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SimpleTodoList.Mobile.ViewModels
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItemDTO> TodoItems { get; set; }
+        public string NewTodoInputValue { get; set; }
+
+        public ICommand AddNewTodoItemCommand { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItemDTO>();
+            AddNewTodoItemCommand = new Command(CreateNewTodoItem); 
+        }
+
+        public void CreateNewTodoItem()
+        {
+            var todoItem = new TodoItemDTO()
+            {
+                Id = 1,
+                Description = NewTodoInputValue,
+                IsComplete = false,
+                IsDeleted = false,
+            }; 
+            TodoItems.Add(todoItem);    
+        }
+    }
+}
